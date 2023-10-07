696788a1dbee3a5663a39183122ad8a9cee2519e
Robert Craig <rcraig12@gmail.com>
2023-10-07 23:50:04 +0100

    reorganised BBC palette order

--- src/palette/bbc.ts
+++ src/palette/bbc.ts
@@ -1,10 +1,10 @@
 export const bbcPallete = [
-  { name: "Red" , colorCode: "#cc3500"},
-  { name: "Green" , colorCode: "#5ec809"},
-  { name: "Blue" , colorCode: "#1d286f"},
-  { name: "Cyan" , colorCode: "#00c4ff"},
-  { name: "Magenta" , colorCode: "#ff00e5"},
-  { name: "Yellow" , colorCode: "#ffe052"},
   { name: "Black" , colorCode: "#000000"},
+  { name: "Red" , colorCode: "#ff0000"},
+  { name: "Green" , colorCode: "#00ff00"},
+  { name: "Yellow" , colorCode: "#ffff00"},
+  { name: "Blue" , colorCode: "#0000ff"},
+  { name: "Magenta" , colorCode: "#ff00ff"},
+  { name: "Cyan" , colorCode: "#00ffff"},  
   { name: "White" , colorCode: "#ffffff"}
 ];
